1e35e86458fbddccd1ce330f3ab7cef4599fbf9c
i_kayneuriel.rodrigo@stratpoint.com <i_kayneuriel.rodrigo@stratpoint.com>
2025-07-04 10:53:49 +0800

    feat: Restructure project with correct directory layout

--- src/com/hospital/strategy/SeniorBillingStrategy.java
+++ src/com/hospital/strategy/SeniorBillingStrategy.java
@@ -3,5 +3,7 @@ package com.hospital.strategy;
 // Andre will implement this
 public class SeniorBillingStrategy implements BillingStrategy {
     // To be implemented: the calculate method
+    // test
+    // test
 }
 // Create similar empty files for CashBillingStrategy and SeniorBillingStrategy
